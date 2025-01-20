713817688e93a216557ee2412255b2603c205260
kuchibiruobake <planning207@gmail.com>
2025-01-20 10:14:52 +0900

    Edit修正

--- CauseConnect/src/views/Detail/components/EditMenu/ParticipantsInfo.vue
+++ CauseConnect/src/views/Detail/components/EditMenu/ParticipantsInfo.vue
@@ -70,27 +70,29 @@ onMounted(() => {
     <section v-if="requester">
       <h4>依頼者</h4>
       <p>ID: {{ requester.user_id }} / ニックネーム: {{ requester.nickname }}
-      <span v-if="requesterSupPoint"> / 依頼ポイント: {{ requesterSupPoint }}</span></p>
+      <span v-if="requesterSupPoint"> / 出資ポイント: {{ requesterSupPoint }}</span></p>
     </section>
 
     <!-- ✅ 出資者の表示 -->
     <section>
       <h4>出資者</h4>
-      <ul>
+      <ul v-if="contributors.length > 1">
         <li v-for="contributor in contributors" :key="contributor.user_id">
           ID: {{ contributor.user_id }} / ニックネーム: {{ contributor.nickname }} / 出資ポイント: {{ contributor.sup_point }}
         </li>
       </ul>
+      <p v-else>なし</p>
     </section>
 
     <!-- ✅ 実行者の表示 -->
     <section>
       <h4>実行者</h4>
-      <ul>
+      <ul v-if="executors.length > 1">
         <li v-for="executor in executors" :key="executor.user_id">
           ID: {{ executor.user_id }} / ニックネーム: {{ executor.nickname }} / 役割: {{ executor.leader === 1 ? 'リーダー' : 'メンバー' }}
         </li>
       </ul>
+      <p v-else>なし</p>
     </section>
   </div>
 </template>
